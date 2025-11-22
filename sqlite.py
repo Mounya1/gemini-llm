9bb5cc1379b6e8b0483d815ad1ff511d396f9be7
Mounya1 <mounyainampudi@gmail.com>
2025-11-21 20:25:37 -0500

    updated

---
+++ sqlite.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection=sqlite3.connect('student.db')
+#create a cursor object to insert record, create table
+cursor=connection.cursor()
+## create the table
+table_info="""
+CREATE TABLE STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),SECTION VARCHAR(25));
+"""
+cursor.execute(table_info)
+#insert records
+cursor.execute("INSERT INTO STUDENT VALUES('MOUNIKA','TENTH','A')")
+cursor.execute("INSERT INTO STUDENT VALUES('ANUSHA','NINTH','B')") 
+cursor.execute("INSERT INTO STUDENT VALUES('SOWMYA','EIGHTH','C')")
+cursor.execute("INSERT INTO STUDENT VALUES('KAVYA','SEVENTH','A')")
+cursor.execute("INSERT INTO STUDENT VALUES('LAKSHMI','SIXTH','B')")
+
+
+#display the records
+print("STUDENT RECORDS:")
+cursor.execute("SELECT * FROM STUDENT")
+for row in cursor.fetchall():
+    print(row)
